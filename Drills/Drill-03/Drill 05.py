ac05c0608388b3139d96fe2064fb65ebe2352b6f
jjangsu <a01056386069@gmail.com>
2018-09-18 18:36:16 +0900

    move_to_xy() 생성
    캐릭터가 움직이는 함수의 뼈대를 만들고 animation_sheet를 불러옴

--- Drills/Drill-03/Drill 05.py
+++ Drills/Drill-03/Drill 05.py
@@ -3,8 +3,9 @@ from pico2d import *
 open_canvas()
 
 grass = load_image('grass.png')
-character = load_image('character.png')
+character = load_image('animation_sheet.png')
 
-
+def move_to_xy():
+    pass
 
 close_canvas()
