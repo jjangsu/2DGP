6349e847b6d9eaf1d5690c096dd930c1e9b6059d
jjangsu <a01056386069@gmail.com>
2018-09-18 18:42:03 +0900

    move함의 안에서 실행될 함수들
    내가 원하는 좌표로 움직일 함수들의 뼈대를 완성

--- Drills/Drill-03/Drill 05.py
+++ Drills/Drill-03/Drill 05.py
@@ -5,7 +5,41 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
 
-def move_to_xy():
+def go_to_132_243():
     pass
+def go_to_535_470():
+    pass
+def go_to_477_203():
+    pass
+def go_to_715_136():
+    pass
+def go_to_316_225():
+    pass
+def go_to_510_92():
+    pass
+def go_to_692_518():
+    pass
+def go_to_682_336():
+    pass
+def go_to_712_349():
+    pass
+def go_to_203_535():
+    pass
+
+def move_to_xy():
+    go_to_132_243()
+    go_to_535_470()
+    go_to_477_203()
+    go_to_715_136()
+    go_to_316_225()
+    go_to_510_92()
+    go_to_692_518()
+    go_to_682_336()
+    go_to_712_349()
+    go_to_203_535()
+    pass
+
+while True:
+    move_to_xy()
 
 close_canvas()
