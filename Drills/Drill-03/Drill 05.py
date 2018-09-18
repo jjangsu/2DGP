f564f5f50f7367c044638b1db2507b0665cca881
jjangsu <a01056386069@gmail.com>
2018-09-18 18:25:21 +0900

    make_circle() 완성
    하단의 중심에서 원궤도로 도는 함수를 완성

--- Drills/Drill-03/Drill 05.py
+++ Drills/Drill-03/Drill 05.py
@@ -5,7 +5,7 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-def move_from_center_toright():
+def move_from_center_to_right():
     x, y = 800 // 2, 90
     while x < 800 - 25:
         clear_canvas_now()
@@ -51,17 +51,32 @@ def    move_left_to_center():
         delay(0.01)
 
 def make_rectangle():
-    # move_from_center_toright()
-    # move_up()
-    # move_left()
-    # move_down()
+    move_from_center_to_right()
+    move_up()
+    move_left()
+    move_down()
     move_left_to_center()
 
+import math
+
 def make_circle():
+    cx, cy, r = 800 // 2, 600 // 2, (600 - 180) // 2
+    degree = - 90
+    while degree < 270:
+        radian = math.radians(degree)
+        x = cx + r * math.cos(radian)
+        y = cy + r * math.sin(radian)
+
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        degree += 1
+        delay(0.01)
+
     pass
 
 while True:
-    make_rectangle()
+   #  make_rectangle()
     make_circle()
 
 close_canvas()
