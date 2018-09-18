36092586c6a4d9e7c759a8bc597648e658605740
jjangsu <a01056386069@gmail.com>
2018-09-18 19:49:54 +0900

    go_to_692_518() 완성
    go_to_692_518() 완성 했다.

--- Drills/Drill-03/Drill 05.py
+++ Drills/Drill-03/Drill 05.py
@@ -113,7 +113,23 @@ def go_to_510_92():
 
 
 def go_to_692_518():
-    pass
+    x, y = 510, 136
+    frame = 0
+    pitureY = 1
+    move_ratio_x = (510 - 316) // 40
+    move_ratio_y = (518 - 136) // 40
+
+    while (x <= 692 and y <= 518):
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, pitureY * 100, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 10
+        x += move_ratio_x
+        y += move_ratio_y
+        delay(0.02)
+
+
 def go_to_682_336():
     pass
 def go_to_712_349():
@@ -127,7 +143,7 @@ def move_to_xy():
     # go_to_477_203()
     # go_to_715_136()
     # go_to_316_225()
-    go_to_510_92()
+    # go_to_510_92()
     go_to_692_518()
     go_to_682_336()
     go_to_712_349()
