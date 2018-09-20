ee73d4f8945a67e0d1b719e1980ff45ada3e9f8f
jjangsu <a01056386069@gmail.com>
2018-09-20 19:04:53 +0900

    Drill 06
    함수를 만들었는데 함수가 작동하지 않아 고치던 중 어디부터 잘못되었는지 찾기 어려워 우선 함수를 없애고 만들었다.
    캐릭터의 피봇이 우선 솜 이미지의 중심에 오도록했다.

--- Drills/Drill-06/Drill 06.py
+++ Drills/Drill-06/Drill 06.py
@@ -19,25 +19,8 @@ def handle_events():
            character_dir = 1
        elif event.type == SDL_MOUSEBUTTONDOWN:
            final_x, final_y = event.x, KPU_HEIGHT - 1 - event.y
-
-           character_move(final_x, final_y)
        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
            running = False
-
-
-def character_move(final_x, final_y):
-    global character_x, character_y
-    move_x = (final_x - character_x) / 20
-    move_y = (final_y - character_y) / 20
-
-    character_x = character_x + move_x
-    character_y = character_y + move_y
-
-    while move_x == final_x and move_y == final_y:
-        character.clip_draw(frame * 100, 100 * character_dir, 100, 100, character_x, character_y)
-
-
-
 
 
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
@@ -52,15 +35,23 @@ frame = 0
 hide_cursor()
 character_dir = 3
 
+final_x = character_x
+final_y = character_y
+
 
 while running:
+    move_x = (final_x - character_x) / 20
+    move_y = (final_y - character_y) / 20
+
+    character_x += move_x
+    character_y += move_y
+
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
     mouse.clip_draw(0, 0, 50, 50, x, y)
     character.clip_draw(frame * 100, 100 * character_dir, 100, 100, character_x, character_y)
     update_canvas()
     frame = (frame + 1) % 8
-
     delay(0.02)
     handle_events()
 
