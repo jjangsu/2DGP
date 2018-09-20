3faf8e7bdfe5638d4f5c8ca55d42dbd155c43b75
jjangsu <a01056386069@gmail.com>
2018-09-20 18:48:33 +0900

    Drill 06
    마우스 클릭을 하면 계속적이지는 않지만 마우스 방향으로 따라오는 것까지 구현

--- Drills/Drill-06/Drill 06.py
+++ Drills/Drill-06/Drill 06.py
@@ -8,6 +8,7 @@ def handle_events():
    global x, y
    global character_dir
    global final_x, final_y
+   global charcter_x, character_y
 
    events = get_events()
    for event in events:
@@ -18,17 +19,26 @@ def handle_events():
            character_dir = 1
        elif event.type == SDL_MOUSEBUTTONDOWN:
            final_x, final_y = event.x, KPU_HEIGHT - 1 - event.y
+
+           character_move(final_x, final_y)
        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
            running = False
 
 
 def character_move(final_x, final_y):
-    move_x =  final_x / 10
-    move_y = final_y / 10
+    global character_x, character_y
+    move_x = (final_x - character_x) / 20
+    move_y = (final_y - character_y) / 20
 
-    character.clip_draw(frame * 100, 100 * character_dir, 100, 100, character_x, character_y)
+    character_x = character_x + move_x
+    character_y = character_y + move_y
 
-    
+    while move_x == final_x and move_y == final_y:
+        character.clip_draw(frame * 100, 100 * character_dir, 100, 100, character_x, character_y)
+
+
+
+
 
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
 kpu_ground = load_image('KPU_GROUND.png')
@@ -47,6 +57,7 @@ while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
     mouse.clip_draw(0, 0, 50, 50, x, y)
+    character.clip_draw(frame * 100, 100 * character_dir, 100, 100, character_x, character_y)
     update_canvas()
     frame = (frame + 1) % 8
 
