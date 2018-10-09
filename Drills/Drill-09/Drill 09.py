95df952a8ed4b899ab0517b4e4cdf2a4bee49ab4
jjangsu <a01056386069@gmail.com>
2018-10-09 18:46:01 +0900

    balls animation
    큰공과 작은공이 하늘에서 떨어지는 모습을 완성하였다.

--- Drills/Drill-09/Drill 09.py
+++ Drills/Drill-09/Drill 09.py
@@ -5,36 +5,50 @@ import random
 class Grass:
     def __init__(self):
         self.image = load_image('grass.png')
-
     def draw(self):
         self.image.draw(400, 30)
 
 class Boy:
     def __init__(self):
         self.x, self.y = random.randint(100, 700), 90
-        self.frame = random.randint(0, 7)
+        self.frame = 0
         self.image = load_image('run_animation.png')
 
     def update(self):
-        self.frame = (self.frame + 1) % 8
+        self.frame = random.randint(0, 7) #  (self.frame + 1) % 8
         self.x += 5
 
     def draw(self):
-        self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
+        self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
 
-class smallBall:
+class Small_Ball:
     def __init__(self):
-        self.x, self.y = random.randint(50, 700), 599
-        self.image = load_image('ball21x21/png')
-        self.move.y = random.randomint(5, 10)
+        self.x, self.y = random.randint(100, 700), 599
+        self.image = load_image('ball21x21.png')
+        self.move_y = random.randint(5, 10)
 
     def update(self):
         self.y -= self.move_y
         if self.y <= 65:
             self.y = 65
 
-class bigBall:
-    pass
+    def draw(self):
+        self.image.clip_draw(0, 0, 21, 21, self.x, self.y)
+
+class Big_Ball:
+    def __init__(self):
+        self.x, self.y = random.randint(100, 700), 599
+        self.image = load_image('ball41x41.png')
+        self.move_y = random.randint(5, 10)
+
+    def update(self):
+        self.y -= self.move_y
+        if self.y <= 65:
+            self.y = 65
+
+    def draw(self):
+        self.image.clip_draw(0, 0, 41, 41, self.x, self.y)
+
 
 def handle_events():
     global running
@@ -51,23 +65,35 @@ open_canvas()
 small_num = random.randint(5, 10)
 big_num = 20 - small_num
 
-
 team = [Boy() for i in range(11)]
+
+small_balls = [Small_Ball() for i in range(small_num)]
+big_balls = [Big_Ball() for i in range(big_num)]
+
 # boy = Boy()
 grass = Grass()
 
-running = True
+running = True;
+
 # game main loop code
 while running:
     handle_events()
 
     for boy in team:
         boy.update()
+    for ball in small_balls:
+        ball.update()
+    for ball in big_balls:
+         ball.update()
 
     clear_canvas()
     grass.draw()
     for boy in team:
         boy.draw()
+    for ball in small_balls:
+        ball.draw()
+    for ball in big_balls:
+        ball.draw()
     update_canvas()
 
     delay(0.05)
