89679861a263c68467418dfdcfdb5ef3a63b2e1c
jjangsu <a01056386069@gmail.com>
2018-10-11 18:52:16 +0900

    Drill 10
    pause class make

---
+++ Drills/Drill-10/pause_state.py
@@ -0,0 +1,39 @@
+from pico2d import *
+
+class Pause:
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+
+def enter():
+    pass
+
+
+def exit():
+    pass
+
+
+def update():
+    pass
+
+
+def draw():
+    pass
+
+
+def handle_events():
+    pass
+
+
+def pause():
+    pass
+
+
+def resume():
+    pass
