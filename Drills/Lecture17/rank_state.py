63b3576a6c616b641e61b71c877cb8545fedfeef
jjangsu <a01056386069@gmail.com>
2018-11-27 18:41:38 +0900

    drill start

---
+++ Drills/Lecture17/rank_state.py
@@ -0,0 +1,69 @@
+import random
+import json
+import pickle
+import os
+
+from pico2d import *
+import game_framework
+import game_world
+import main_state
+import world_build_state
+
+name = "LankingState"
+
+ranking_list = None
+font = None
+image = None
+
+def enter():
+    global ranking_list, game_rank, font, image
+    image = load_image('menu.png')
+    font = load_font('ENCR10B.TTF', 20)
+
+    with open('rank.json', 'rb') as f:
+        ranking_list = json.load(f)
+
+    game_rank = None
+
+    for data in ranking_list:
+        if data['score'] < main_state.game_record:
+            game_rank = data['rank']
+            i = 9
+            while i >= game_rank:
+                ranking_list[game_rank]['score'] = ranking_list[game_rank - 1]['score']
+                i -= 1
+            data['score'] = main_state.game_record
+            break
+    rank_list = json.dumps(ranking_list)
+    f = open("rank.json", 'w')
+    f.write(rank_list)
+    f.close()
+
+def update():
+    pass
+
+def draw():
+    global ranking_list
+    global font, image
+    clear_canvas()
+
+    image.clip_draw(0, 0, 10, 10, 600, 450, 800, 400)
+
+    i = 600
+    for data in ranking_list:
+        font.draw(400, i, '#%d' % data['rank'], (0, 0, 0))
+        font.draw(500, i, 'record: %3f' % data['score'], (0, 0, 0))
+        i -= 30
+
+    update_canvas()
+
+def exit():
+    pass
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.change_state(world_build_state)
